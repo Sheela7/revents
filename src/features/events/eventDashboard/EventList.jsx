3b78637a95cb051a9f1bd3822431ff7c9af77024
sheela7 <sheela7>
2022-07-27 21:49:45 +0545

    .

--- src/features/events/eventDashboard/EventList.jsx
+++ src/features/events/eventDashboard/EventList.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import EventListItem from "./EventListitem";
 
-export default function EventList({events, selectEvent, deleteEvent}){
-    return(
-        <>
-        {events.map(event => (
-            <EventListItem  event={event} key={event.id} selectEvent={selectEvent} deleteEvent={deleteEvent}/>
-        ))}
-        
-        {/* <EventListItem />
+export default function EventList({ events, selectEvent, deleteEvent }) {
+  return (
+    <>
+      {events.map((event) => (
+        <EventListItem
+          event={event}
+          key={event.id}
+          selectEvent={selectEvent}
+          deleteEvent={deleteEvent}
+        />
+      ))}
+
+      {/* <EventListItem />
         <EventListItem />
         <EventListItem />
         <EventListItem /> */}
-        </>
-    )
+    </>
+  );
 }
